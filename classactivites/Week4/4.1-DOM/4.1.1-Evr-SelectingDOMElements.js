6c6d8c411f973a6e9e3168e95fec25a350be16ad
Karmen Beers <Karmenbeers@gmail.com>
2021-09-13 21:58:25 -0500

    4.1activites

---
+++ classactivites/Week4/4.1-DOM/4.1.1-Evr-SelectingDOMElements.js
@@ -0,0 +1,8 @@
+// select all unordered list (ul) elements
+console.log(document.getElementsByTagName("ul"))
+console.log(document.querySelectorAll("ul")
+// select all elements with the class "class-week"
+// console.log(document.getElements
+// select all elements with the class "nav-bar"
+
+// select the element with the id "dog-picture" and save it to a variable
---
+++ week3homework/week3.js
@@ -0,0 +1,22 @@
+let pizzaToppings = ["pineapples", "chicken", "banana peppers", "bbq sauce"];
+function greetCustomer() {
+  let greeting =
+    " Welcome to Peekers Pizza, We offer many great toppings like ";
+  for (let topping of pizzaToppings) {
+    greeting += `${topping}, `;
+  }
+  console.log(greeting);
+}
+greetCustomer();
+
+function getPizzaOrder(size, crust, ...toppings) {
+  console.log(`one large thick crust pizza with ${pizzaToppings}`);
+}
+getPizzaOrder();
+
+function preparePizza "large" "thincrust" "cheese"
+let preparePizza = cooking pizza
+
+
+let servePizza = {
+
