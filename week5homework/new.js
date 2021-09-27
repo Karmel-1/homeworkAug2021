6065dc12eebe8f74be83f47cf27f4553fe1bc0b2
Karmen Beers <Karmenbeers@gmail.com>
2021-09-27 17:27:33 -0500

    week5homework

---
+++ week5homework/new.js
@@ -0,0 +1,10 @@
+class User{
+  constructor (GameConsoles, VideoGames ){
+          this.GameConsoles=GameConsoles;
+          this.VideoGames=VideoGames ;
+  }
+}
+
+var itemOne = new item (nintendo ds, SuperMario, $484.99)
+var itemTwo = new item (xbox, fifa22, $540)
+}id, name, or price.
